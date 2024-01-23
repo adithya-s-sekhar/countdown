281067b5d3edf07c6cb7f28ffe62cd395c506b88
Adithya S Sekhar <adithya0332@gmail.com>
2024-01-23 12:58:58 +0530

    u\

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ function App() {
   }
 
   function addZeroIfUnderHundred(value){
-    if(value<100){
-      return '0'+value;
-    } else if(value<10){
+    if(value<10){
       return '00'+value;
+    } else if(value<100){
+      return '0'+value;
     } else{
       return value;
     }
